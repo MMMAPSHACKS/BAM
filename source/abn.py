d3c46e11ddfb134e1e31207a6aafbe3ee05e6c39
mleadbeater <mleadbeater@me.com>
2015-07-04 16:03:19 +0930

    abn data import - process files given on the commandline

--- source/abn.py
+++ source/abn.py
@@ -1,7 +1,7 @@
+import sys
 import xml.etree.cElementTree as ET
-#import xml.etree.ElementTree as ET
 
-source = '20150701_Public01.xml'
+#source = '20150701_Public01.xml'
 #context = ET.iterparse(source, events=("start", "end"))
 #context = iter(context)
 #event, root = context.next()
@@ -9,52 +9,63 @@ source = '20150701_Public01.xml'
 #for event, elem in context:
 #	print event, elem.tag
 
-root = None
-cols=[]
-vals={}
-depth = 0
-last_header = ''
-all_keys = []
-for event, elem in ET.iterparse(source, events=("start", "end")):
-	if event == "start":
-		#print 'S ' + elem.tag
-		if root == None:
-			root = elem
-		cols.insert(0,elem.tag)
-		depth += 1
-		vals[elem.tag] = ''
-		if all_keys.count(elem.tag) == 0:
-			all_keys.append(elem.tag)
-	else:
-		#print 'E ' + elem.tag, elem.attrib, elem.text
-		depth -= 1
-		vals[elem.tag] = elem.text
-		if depth == 1:
-			line = ''
-			delim = ''
-			for col in all_keys:
-				line = line + delim + col
-				delim = '\t'
-			if line != last_header:
-				last_header = line
-				print last_header
-			if vals.get('State','') == 'SA':
+def render_file( source, all_keys ):
+	root = None
+	cols=[]
+	vals={}
+	depth = 0
+	last_header = ''
+	for event, elem in ET.iterparse(source, events=("start", "end")):
+		if event == "start":
+			#print 'S ' + elem.tag
+			if root == None:
+				root = elem
+			cols.insert(0,elem.tag)
+			depth += 1
+			vals[elem.tag] = ''
+			if all_keys.count(elem.tag) == 0:
+				all_keys.append(elem.tag)
+		else:
+			#print 'E ' + elem.tag, elem.attrib, elem.text
+			depth -= 1
+			vals[elem.tag] = elem.text
+			if depth == 1:
 				line = ''
 				delim = ''
 				for col in all_keys:
-					v = vals.get(col, '')
-					if v == None:
-						v = ''
-					line = line + delim + v
+					line = line + delim + col
 					delim = '\t'
-				print line
-			cols = [ col[0] ]
-			vals = {}
-			vals[cols[0]] = elem.text
-
+				if line != last_header:
+					last_header = line
+					print last_header
+				pcode = vals.get('Postcode', '')
+				if vals.get('State','') == 'SA' and (pcode == '5000' or pcode == '5006'):
+					line = ''
+					delim = ''
+					for col in all_keys:
+						v = vals.get(col, '')
+						if v == None:
+							v = ''
+						line = line + delim + v
+						delim = '\t'
+					print line
+				cols = [ col[0] ]
+				vals = {}
+				vals[cols[0]] = elem.text
+	
 #tree = ET.parse('country_data.xml')
 #root = tree.getroot()
-
+	
 #print root.tag, root.attrib
 #for child in root:
 #	print child.tag, child.attrib
+
+
+all_keys = []
+n = 0
+for arg in sys.argv:
+	n += 1
+	if n == 1:
+		continue
+	render_file( arg, all_keys )
+
